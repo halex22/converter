e7f953845f3815ac8ba44635e3810e2e687f6ba1
halex22 <73904950+halex22@users.noreply.github.com>
2023-07-30 15:56:20 +0200

    start adding some functions

--- index.js
+++ index.js
@@ -2,3 +2,35 @@ function MouseReaction(x) {
     $(x).toggleClass("mouse-over-input")
 }
 
+function Calculate(value) {
+    const inputUnit = $("#Partenza").val();
+    const givenValue = $("#inputEntered")
+
+    let resultValue;
+    switch (inputUnit) {
+        case "mg/mL":
+            resultValue = mgmLToM()
+    }
+}
+
+function convertUnit(input, conversionFactor) {
+    const wm = $("#wm").val();
+    const result = input / wm * conversionFactor;
+    return result;
+}
+
+function gmLToM(input) {
+    return convertUnit(input, 1e3); 
+}
+
+function mgmLToM(input) {
+    return convertUnit(input, 1); 
+}
+
+function mgmLTouM(input) {
+    return convertUnit(input, 1e6);
+}
+
+function mgmLTomM(input) {
+    return convertUnit(input, 1e3);
+}
