cc167640560a402c4d11e3458a37c520414ba92b
halex22 <73904950+halex22@users.noreply.github.com>
2023-10-29 22:05:23 +0100

    commit with jsdoc reference

--- converter_vs.js
+++ converter_vs.js
@@ -1,11 +1,20 @@
+/**
+ * @typedef {Object.<string, {value: number, useMW: boolean}>} unitMap
+ */
+
+/**
+ * @type {unitMap}
+ */
 const unitMap = {
-    "g/ml-m": { value: 1000, useMW: true },
+    "g/ml-m": { value: 1e3, useMW: true },
+    "g/ml-mm": {value: 1e6, useMW: true},
+    "g/ml-μm": {value: 1e9, useMW: true},
     "mg/ml-m": { value: 1, useMW: true },
-    "g/ml-mg/ml": { value: 0, useMW: false },
-    "mg/ml-μm": { value: 1e-6, useMW: true },
-    "mg/ml-mm": { value: 1e-3, useMW: true },
+    "g/ml-mg/ml": { value: 1e3, useMW: false },
+    "mg/ml-μm": { value: 1e6, useMW: true },
+    "mg/ml-mm": { value: 1e3, useMW: true },
     "m-mg/ml": { value: 1, useMW: true },
-    "m-g/ml": { value: 1e-3, useMW: true },
+    "m-g/ml": { value: 1e3, useMW: true },
     "μm-mg/ml": { value: 1e6, useMW: true },
     "μm-g/ml": { value: 1e3, useMW: true },
     "mm-mg/ml": { value: 1e3, useMW: true },
@@ -13,15 +22,19 @@ const unitMap = {
     "m-μm": { value: 1e6, useMW: false },
     "mm-m": { value: 1e-3, useMW: false },
     "μm-m": { value: 1e-6, useMW: false },
-    "mm-g/ml": { value: 1, useMW: true },
+    "mm-g/ml": { value: 1e6, useMW: true },
     "mm-μm": { value: 1000, useMW: false },
     "μm-mm": { value: 0.001, useMW: false },
     "mg/ml-g/ml": { value: 0.001, useMW: false },
     "g/ml-gm/ml": { value: 1, useMW: false }
 };
 
-function MouseReaction(x) {
-    $(x).toggleClass("mouse-over-input");
+/**
+ * 
+ * @param {HTMLElement} x 
+ */
+function MouseReaction(element) {
+    element.classList.toggle("mouse-over-input");
 }
   
 
@@ -32,6 +45,14 @@ function resetResultArea() {
     });
 }
 
+/**
+ * Converts the given input to the selected measure 
+ * @param {number} input 
+ * @param {number} conversionFactor 
+ * @param {boolean} reverse - true if converting molars to g/ml or mg/ml
+ * @param {boolean} useMW 
+ * @returns {number}
+ */
 function convertUnit(input, conversionFactor, reverse, useMW) {
     const wmValue = document.getElementById("wm").value
     const mw = useMW ? parseFloat(wmValue) : 1
@@ -53,24 +74,47 @@ function Calculate() {
     const givenValue = parseFloat(document.getElementById("inputEntered").value);
     let result
 
+    if (SameInput()) {
+        document.getElementById("sameUnit").
+        textContent = "Unità di partenza e arrivo sono le stesse";
+        return;
+    }
     const conversionFactor = unitMap[unitCombo].value;
     const useMWFactor = unitMap[unitCombo].useMW;
 
-    if (conversionFactor === undefined) {
-        document.getElementById("sameUnit").
-        textContent("Unità di partenza e arrivo sono le stesse");
-        return;
-    }
-    const reverseConversion = unitCombo.startsWith("m-");
-    if (unitCombo === "mm-μm" || unitCombo === "μm-mm") {
+    if (convertBetweenMs(unitCombo.split("-"))) {
         result = convertUnit(givenValue, conversionFactor, false, false);
     } else {
-        result = convertUnit(givenValue, conversionFactor, reverseConversion, useMWFactor);
+        result = convertUnit(givenValue, conversionFactor, true, useMWFactor);
     }
+    writeResult(result);
+}
 
-    // const resultElement = document.getElementById("result") 
-    console.log(result)
-    document.getElementById("result").text = result.toFixed(6);
+/**
+ * Writes the result of the convertion
+ * @param {Number} result 
+ */
+
+function writeResult(result) {
+    document.getElementById("result").textContent = result;
     document.getElementById("resultVar").
     textContent = document.getElementById("arrivo").value;
+}
+
+/**
+ * Checks if a combination contains the letter 'm'.
+ * @param {string[]} combo An array representing a combination.
+ * @returns {boolean} - True if 'm' is present in the combination, false otherwise.
+ */
+function convertBetweenMs(combo) {
+    return combo[0] === "m" || combo[1][1] == "m" || combo[1] === "m";
+}
+
+/**
+ * 
+ * @returns {boolean} - True if the inputs are the same
+ */
+function SameInput() {
+    const comboArray = createComboUnit().split("-");
+    return comboArray[0] === comboArray[1];
 }
